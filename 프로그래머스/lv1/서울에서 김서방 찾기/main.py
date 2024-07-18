5ad428a605a6deba33f2c817da95c7602a992f76
황광회 <jack7141@nate.com>
2024-07-18 23:03:11 +0900

    [level 1] Title: 서울에서 김서방 찾기
    list.index('찾고자하는값') = 인덱스값 위치

---
+++ 프로그래머스/lv1/서울에서 김서방 찾기/main.py
@@ -0,0 +1,9 @@
+def solution(seoul):
+    answer = f'김서방은 {seoul.index("Kim")}에 있다'
+    return answer
+
+seoul = ["Jane", "Kim"]
+print(solution(seoul))
+
+# list.index('찾고자하는값') = 인덱스값 위치
+# https://dreamjy.tistory.com/93
