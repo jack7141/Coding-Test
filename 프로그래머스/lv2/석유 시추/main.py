c37195043a63c4ed0a604578b40a06ad40d5e417
황광회 <jack7141@nate.com>
2024-08-18 19:16:26 +0900

    [level 2] Title: 석유 시추 BFS

--- 프로그래머스/lv2/석유 시추/main.py
+++ 프로그래머스/lv2/석유 시추/main.py
@@ -1,55 +1,41 @@
 from collections import deque
 
 
-directions = [(0, 1), (1, 0), (-1, 0), (0, -1)]
 
 import numpy as np
 def solution(land):
-    answer = 0
     n, m = len(land), len(land[0])
-
-    # 방문배열
+    directions = [(0, 1), (1, 0), (-1, 0), (0, -1)]
     visited = [[False] * m for _ in range(n)]
-    # 각 열의 기름을 얼마나 추출할 수 있는지 세팅
-    oil = [0] * m
-    directions = [(0, 1), (1, 0),
-                  (-1, 0), (0, -1)]
-
-    def bfs(row, col):
-        queue = deque()
-        queue.append([row, col])
-        visited[row][col] = True
-        cnt = 1
-        # bfs 탐색중 석유가 있는 열, 중복을 방지하기 위한 set
-        oil_covered = {col}
-
+    answer = [0] * m
+    def bfs(x, y):
+        queue = deque([[x, y]])
+        visited[x][y] = True
+        oil_count = 1
+        oil_covered = {y}
         while queue:
             x, y = queue.popleft()
             for dx, dy in directions:
                 nx = x + dx
                 ny = y + dy
-                if 0 <= nx < n and 0 <= ny < m and land[nx][ny] == 1 and not visited[nx][ny]:
+                if 0 <= nx < n and 0 <= ny < m and land[nx][ny] == 1 and visited[nx][ny] == False:
                     queue.append([nx, ny])
                     visited[nx][ny] = True
-                    cnt += 1
-                    # 현재 석유를 발견한 열 추가
+                    oil_count += 1
                     oil_covered.add(ny)
 
-        # 각 열을 돌면서 석유량 추가
-        for c in oil_covered:
-            oil[c] += cnt
+        for i in oil_covered:
+            answer[i] += oil_count
+        return True
 
-    # bfs 탐색
-    # 행
     for i in range(n):
-        # 열
         for j in range(m):
-            if land[i][j] == 1 and not visited[i][j]:
+            if land[i][j] == 1 and visited[i][j] == False:
                 bfs(i, j)
 
-    answer = max(oil)
 
-    return answer
+
+    return max(answer)
 
 test_cases = [
     [[0, 0, 0, 1, 1, 1, 0, 0],
@@ -67,25 +53,3 @@ for land in test_cases:
     print(solution(land))
 
 
-
-
-## BFS 원형
-
-"""
-def dfs(x, y)
-    queue = deque()
-    queue.append([x, y])
-    while queue:
-        x, y = queue.popleft()
-        for dx, dy in directions:
-            nx = x + dx
-            ny = y + dy
-            if 0 <= nx < n and 0 <= ny < m:
-                queue.append([nx, ny])
-
-
-for x in range(n):
-    for y in range(m):
-        bfs(x, y)
-        
-"""
